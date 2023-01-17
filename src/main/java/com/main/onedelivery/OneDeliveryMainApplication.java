168cddd8d2a1c6f358be8082d26e7cbca5b05481
Roman Sulymka <roma.sulimak@gmail.com>
2023-01-17 22:45:58 +0200

    Initial commit

---
+++ src/main/java/com/main/onedelivery/OneDeliveryMainApplication.java
@@ -0,0 +1,13 @@
+package com.main.onedelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneDeliveryMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneDeliveryMainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/main/onedelivery/OneDeliveryMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.onedelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneDeliveryMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
